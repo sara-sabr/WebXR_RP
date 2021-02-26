c30ce6ae7669d5c6d5f11d1edf66bbf307d1bb70
marinierb <55813407+marinierb@users.noreply.github.com>
2021-02-26 12:49:15 -0500

    Fixes to French

--- src/assets/locale/fr.js
+++ src/assets/locale/fr.js
@@ -3,18 +3,18 @@ import HelloMessage from '../../assets/audio/Hello.mp3';
 const fr = {
   translation: {
     // Intro
-    'intro.dialog': 'Visez le plancher pour faire apparaitre le kiosque.',
+    'intro.dialog': 'Visez le plancher pour faire apparaître le kiosque.',
     // Welcome
     'agent.welcome.dialog':
-      "Bienvenue au prototype RA de l’équipe recherche et prototypage. Je m'appelle Simon, vôtre agent virtuel. Comment puis-je vous aider?",
+      "Bienvenue au prototype de RA de l’équipe de recherche et prototypage. Je m'appelle Simon, votre agent virtuel. Comment puis-je vous aider?",
     'agent.welcome.audio': HelloMessage,
     // Service
     'agent.service.etms.dialog':
-      'Vous aviez sélectionné le service de surveillances des technologies émergentes.',
+      'Vous avez sélectionné le service de surveillances des technologies émergentes.',
     // Landing Page
-    't-header': 'Recherce & Prototypage - Prototype WebXR',
+    't-header': 'Recherche & prototypage - Prototype WebXR',
     't-blurb':
-      "Il s'agit d'un prototype et n'implique aucune possibilité d'un tel service à EDSC où la réalité augmentée est disponible ou offerte.\n\n L'objectif de ce prototype est de faire évoluer les limites et de voir ce qu'une technologie donnée pourrait faire.",
+      "Il s'agit d'un prototype et n'implique en aucune façon qu'un tel service à EDSC sera offert où la réalité augmentée est disponible.\n\n L'objectif de ce prototype est de faire évoluer les limites et de voir ce qu'une technologie donnée pourrait faire.",
     't-device': "Exigences minimales de l'appareil",
     't-install':
       'Installer <strong><a href="https://apps.apple.com/us/app/webxr-viewer/id1295998056">WebXR Viewer</a></strong>',
@@ -22,7 +22,7 @@ const fr = {
     't-pcInstall':
       '<strong>Chrome 87+</strong> avec <strong><a href="https://chrome.google.com/webstore/detail/webxr-api-emulator/mjddjgeghkdijejnciaefnkjmkafnnje?hl=fr">WebXR API Emulator</a></strong> installé et configuré.',
     't-preBtn':
-      "Sélectionnez le bouton ci-dessous pour lancer l'application RA !",
+      "Sélectionnez le bouton ci-dessous pour lancer l'application de RA!",
   },
 };
 
