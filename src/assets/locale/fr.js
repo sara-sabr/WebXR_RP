29c94a7ff516b244e6956088382e76d3151462a6
“Jdure” <22172148+Jdure@users.noreply.github.com>
2021-02-26 10:11:51 -0500

    french translated

--- src/assets/locale/fr.js
+++ src/assets/locale/fr.js
@@ -10,6 +10,17 @@ const fr = {
     'agent.welcome.audio': HelloMessage,
     // Service
     'agent.service.ei.dialog': 'I see you need help with EI, ...-FR',
+    't-header': 'Recherce & Prototypage - Prototype WebXR',
+    't-blurb':
+      "Il s'agit d'un prototype et n'implique aucune possibilité d'un tel service à EDSC où la réalité augmentée est disponible ou offerte.\n\n L'objectif de ce prototype est de faire évoluer les limites et de voir ce qu'une technologie donnée pourrait faire.",
+    't-device': "Exigences minimales de l'appareil",
+    't-install':
+      'Installer <strong><a href="https://apps.apple.com/us/app/webxr-viewer/id1295998056">WebXR Viewer</a></strong>',
+    't-desktop': 'Ordinateur de bureau',
+    't-pcInstall':
+      '<strong>Chrome 87+</strong> avec <strong><a href="https://chrome.google.com/webstore/detail/webxr-api-emulator/mjddjgeghkdijejnciaefnkjmkafnnje?hl=fr">WebXR API Emulator</a></strong> installé et configuré.',
+    't-preBtn':
+      "Sélectionnez le bouton ci-dessous pour lancer l'application RA !",
   },
 };
 
